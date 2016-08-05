c330506893c7c05050ad926d78f04d89149d9f95
an-pavlovych <an.pavlovych@gmail.com>
2016-08-05 10:15:02 +0300

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+  include Maestrano::Connector::Rails::SessionHelper
+
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
